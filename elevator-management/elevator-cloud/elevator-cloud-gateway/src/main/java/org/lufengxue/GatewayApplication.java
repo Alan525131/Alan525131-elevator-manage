ce0ae79396e30fabeb710ab5cbc2280a8b56de2a
hui <LFX525131>
2022-03-28 20:24:41 +0800

    项目初始化

---
+++ elevator-management/elevator-cloud/elevator-cloud-gateway/src/main/java/org/lufengxue/GatewayApplication.java
@@ -0,0 +1,20 @@
+package org.lufengxue;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 作 者: 陆奉学
+ * 工 程 名:  elevator
+ * 包    名:  org.lufengxue
+ * 日    期:  2022-03-2022/3/28
+ * 时    间:  20:01
+ * 描    述:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+
+    }
+}
